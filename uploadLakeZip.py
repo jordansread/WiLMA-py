00e62e27125121ca3a88bfba884409063515883a
Jordan S Read <jread@usgs.gov>
2013-01-04 16:44:45 -0600

    Used to upload LTER donuts

--- uploadLakeZip.py
+++ uploadLakeZip.py
@@ -11,10 +11,11 @@ Copyright (c) 2013 USGS. All rights reserved.
 import pyGDP
 import os
 
-QA = True #if true, reset locations for services to QA servers
-filePath = '/Volumes/projects/WiLMA/zippedLakes/'
+QA = False #if true, reset locations for services to QA servers
+filePath = '/Volumes/projects/WiLMA/LTER_donuts/'
 
 if QA:
+    print 'using QA servers and services'
     pyGDP.WFS_URL    = 'http://cida-wiwsc-gdp2qa.er.usgs.gov:8082/geoserver/wfs'
     pyGDP.upload_URL = 'http://cida-wiwsc-gdp2qa.er.usgs.gov:8082/geoserver'
     pyGDP.WPS_Service    = 'http://cida.usgs.gov/qa/climate/gdp/utility/WebProcessingService'
@@ -25,18 +26,17 @@ pyGDP 	   = pyGDP.pyGDPwebProcessing()
 
 for filename in os.listdir(filePath):
 	try:
-		shpfile = pyGDP.uploadShapeFile(filePath+filename)
-		print 'successfully uploaded %s' % (shpfile)
+		shpfile = pyGDP.uploadShapeFile(filePath+filename),
+		print 'successfully uploaded %r' % (shpfile)
+		# get attributes
+		att = pyGDP.getAttributes('%s' % (shpfile))
+		val = pyGDP.getValues('%s' % (shpfile),att[1])
+		for v in val:
+			print '%s has attribute %s and value %s' % (shpfile,att[1],v)
 	except Exception, msg:
 		shpfile = filename[0:len(filename)-4]
 		print '%s %s' % (shpfile,msg)
 
-# get attributes
-att = pyGDP.getAttributes(shpfile)
 
-
-val = pyGDP.getValues(shpfile,att[0])
-for v in val:
-	print '%s has attribute %s and value %s' % (shpfile,att[0],v)
 
 
